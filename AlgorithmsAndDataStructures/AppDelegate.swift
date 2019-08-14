7488a15c9054e3f910c3e24fd8b0a2ac73a73e5f
Евгений <leychenkoev@gmail.com>
2019-08-14 14:35:56 +0300

    added initial project

---
+++ AlgorithmsAndDataStructures/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  AlgorithmsAndDataStructures
+//
+//  Created by Евгений on 8/14/19.
+//  Copyright © 2019 Евгений. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        return true
+    }
+}
